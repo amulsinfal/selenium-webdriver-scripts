8a63ef83832296769996e203eb47a9a1f0bb2770
Amul Sinfal <amul.sinfal@gmail.com>
2026-06-21 15:07:28 +0530

    Add LaunchEdgeBrowser.java file.

---
+++ src/test/java/programs/LaunchEdgeBrowser.java
@@ -0,0 +1,34 @@
+package programs;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class LaunchEdgeBrowser {
+
+	public static void main(String[] args) {
+		System.out.println("Program to launch Edge browser");
+
+		WebDriver driver = new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		try {
+			String url = "https://www.google.com";
+			driver.get(url);
+			String actualTitle = driver.getTitle();
+			String expectedTitle = "Google";
+
+			if (actualTitle.equals(expectedTitle)) {
+				System.out.println("Page title is as expected - Test Passed.");
+			} else {
+				System.out.println("Page title is not as expected - Test Failed.");
+			}
+		} catch (Exception e) {
+			throw new RuntimeException("Test execution failed due to: " + e.getMessage());
+		} finally {
+			driver.quit();
+		}
+	}
+}
