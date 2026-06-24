e4d8a88dc396511aa26aff0f60dd4799c2fb9bac
Amul Sinfal <amul.sinfal@gmail.com>
2026-06-24 17:34:39 +0530

    Add DropDown_SelectByValue.java file.

---
+++ src/test/java/programs/DropDown_SelectByValue.java
@@ -0,0 +1,52 @@
+package programs;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class DropDown_SelectByValue {
+
+	public static void main(String[] args) {
+		System.out.println("Program to select option from dropdown by value");
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		try {
+			String url = "https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_select";
+			driver.get(url);
+			driver.switchTo().frame("iframeResult");
+			
+			WebElement carsDropDown = driver.findElement(By.id("cars"));
+			Select selectCars = new Select(carsDropDown);
+			selectCars.selectByValue("saab");	// selectByValue() will select the option with value 'saab'.
+
+			WebElement submitButton = driver.findElement(By.xpath("//input[@value='Submit']"));
+			submitButton.click();
+
+			WebElement result = driver.findElement(By.xpath("//div[@class='w3-container w3-large w3-border']"));
+
+			String actualText = result.getText();
+			String expectedText = "cars=saab ";
+
+			System.out.println("Result Text displayed: " + actualText);
+
+			if (actualText.equals(expectedText)) {
+				System.out.println("Result text displayed as expected - Test Passed.");
+			} else {
+				System.out.println("Result text displayed is not as expected - Test Failed.");
+			}
+
+			driver.switchTo().defaultContent();
+		} catch (Exception e) {
+			throw new RuntimeException("Test execution failed due to: " + e.getMessage());
+		} finally {
+			driver.quit();
+		}
+	}
+}
