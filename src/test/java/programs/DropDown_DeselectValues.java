b756ba9ae0522ac9f367d7b896eef9d3b2cf0b82
Amul Sinfal <amul.sinfal@gmail.com>
2026-06-25 13:40:43 +0530

    Add DropDown_DeselectValues.java file.

---
+++ src/test/java/programs/DropDown_DeselectValues.java
@@ -0,0 +1,64 @@
+package programs;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class DropDown_DeselectValues {
+
+	public static void main(String[] args) {
+		System.out.println("Program to de-select options");
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		try {
+			String url = "https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_select_multiple";
+			driver.get(url);
+			driver.switchTo().frame("iframeResult");
+
+			WebElement carsDropDown = driver.findElement(By.id("cars"));
+			Select selectCars = new Select(carsDropDown);
+
+			System.out.println("Selecting options in dropdown...");
+			selectCars.selectByIndex(1);
+			selectCars.selectByIndex(2);
+			selectCars.selectByIndex(3);
+			List<WebElement> allSelectedOptions = selectCars.getAllSelectedOptions();
+			List<String> actualSelectedOptions = new ArrayList<>();
+			for (WebElement selectedOption : allSelectedOptions) {
+				actualSelectedOptions.add(selectedOption.getText());
+			}
+			System.out.println("Number of options selected: " + actualSelectedOptions.size());
+			System.out.println("Selected Options: " + actualSelectedOptions);
+
+			System.out.println("\nDe-selecting options from dropdown...");
+			for (String selectedOption : actualSelectedOptions) {
+				selectCars.deselectByVisibleText(selectedOption); // deselectByVisibleText() will deselect the options matching the text from the list.
+			}
+			allSelectedOptions = selectCars.getAllSelectedOptions();
+			System.out.println("Number of options selected: " + allSelectedOptions.size());
+			System.out.println("Selected Options: " + allSelectedOptions);
+
+			if (allSelectedOptions.isEmpty()) {
+				System.out.println("All options deselected from dropdown - Test Passed");
+			} else {
+				System.out.println("Some options are still selected - Test Failed.");
+			}
+			
+			driver.switchTo().defaultContent();
+		} catch (Exception e) {
+			throw new RuntimeException("Test execution failed due to: " + e.getMessage());
+		} finally {
+			driver.quit();
+		}
+
+	}
+}
